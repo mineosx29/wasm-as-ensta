7d81318b1c443a6b0c1a6217d4fe63fc86206ad3
mineosx29 <nadir.ziani29@gmail.com>
2023-02-14 09:52:08 +0100

    Modif run compilator

--- Compilator/run.sh
+++ Compilator/run.sh
@@ -1,3 +1,2 @@
 python3 main.py fsm2.cd
-dot -Tpng code.gv -o /home/nadir/Documents/Projet_Application_Système/WASM_Project_Demo/assets/code2.png
-mv cond.txt /home/nadir/Documents/autoweb/WebApp/static/python/
+dot -Tpng code.gv -o ../assets/code2.png
